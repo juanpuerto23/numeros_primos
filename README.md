61c94ed6f86bacf13ff31fb04793e87ee8adef05
juanpuerto23 <juan2224511@correo.uis.edu.co>
2022-08-10 11:46:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Programa independiente #1
+
+Hacer un programa que cuente todos los números primos de 1 a 200, que imprima los primos anteriores a cierto número y que diga si cierto número es primo o no
+
+## Pantallazos
+
+![captura](Pantallazo.jpg)
---
+++ numero_primo_anterior.py
@@ -0,0 +1,40 @@
+numero = 1
+contador = 0
+print("\n-----------------------------------")
+print("---------- NÚMEROS PRIMOS ---------")
+print("-----------------------------------")
+print("\n1.Numeros primos de 1 a 200\n2.Numeros primos anteriores a cierto número\n3.Saber si un número es primo o no")
+opcion = int(input("Eliga una opcion: "))
+
+if opcion == 1:
+    while numero <= 200:
+        contador = 1
+        x = 0
+        while contador <= numero:
+            if numero % contador == 0:
+                x += 1    
+            contador += 1
+        if x == 2:
+            print(numero)
+        numero += 1
+
+if opcion == 2:
+    PedirNumero = int(input("Ingrese su número: "))
+    while numero <= PedirNumero:
+        contador = 1
+        x = 0
+        while contador <= numero:
+            if numero % contador == 0:
+                x += 1    
+            contador += 1
+        if x == 2:
+            print(numero)
+        numero += 1
+
+if opcion == 3:
+    PedirNumero = int(input("Ingrese su número: "))
+    contador = 2
+    if (PedirNumero % contador) == 0:
+        print(PedirNumero, " es un número primo")
+    else:
+        print(PedirNumero, " no es un número primo")
